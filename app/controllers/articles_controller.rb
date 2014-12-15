5fb75f5bb27945b3a0c42fbd26b559ca6c0c9512
ajwithem <ajwithem@gmail.com>
2014-12-15 16:30:18 -0600

    article dir

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,12 @@
 class ArticlesController < ApplicationController
   def new
-    @articles = Article.new
+    @article = Article.new
   end
 
   def create
-    @articles = Article.new(article_params)
+    @article = Article.new(article_params)
 
-    if @articles.save
+    if @article.save
       redirect_to @article
     else
       render 'new'
